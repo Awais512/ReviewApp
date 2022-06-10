1b17ddc95fc5d7a13e2c72b0082ca36540a3fd85
awais512 <raza.awais25@gmail.com>
2022-06-10 17:00:30 +0500

    Forgot Password Form

--- src/components/Auth/ForgotPassword.jsx
+++ src/components/Auth/ForgotPassword.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
+import { Title, Container, FormInput, Submit, CustomLink } from '../';
 
 const ForgotPassword = () => {
-  return <div>ForgotPassword</div>;
+  return (
+    <>
+      <div className='fixed inset-0 bg-primary -z-20 flex justify-center items-center'>
+        <Container>
+          <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+            <Title>Please Enter Your Email</Title>
+
+            <FormInput
+              label='Email'
+              placeholder='Enter Your Email'
+              name='email'
+              type='text'
+            />
+            <Submit value='Send Link' />
+            <div className='flex justify-between'>
+              <CustomLink
+                to='/auth/signin'
+                className='text-dark-subtle hover:text-white transition'
+              >
+                Sign In
+              </CustomLink>
+              <CustomLink
+                to='/auth/signup'
+                className='text-dark-subtle hover:text-white transition'
+              >
+                Sign Up
+              </CustomLink>
+            </div>
+          </form>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default ForgotPassword;
--- src/components/Auth/Signin.jsx
+++ src/components/Auth/Signin.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Container, FormInput, Submit, Title, CustomLink } from '../';
 
 const Signin = () => {
