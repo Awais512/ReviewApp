74f562499a3d56110509975dd02b1b68949b9753
awais512 <raza.awais25@gmail.com>
2022-06-10 11:51:46 +0500

    Navbar UI Completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BsFillSunFill } from 'react-icons/bs';
 const logo = './logo.png';
 
 const Navbar = () => {
@@ -8,8 +9,20 @@ const Navbar = () => {
       <div className='text-white p-2 max-w-screen-xl mx-auto '>
         <div className='flex justify-between items-center'>
           <img className='h-10' src={logo} alt='' />
-          <ul>
-            <li>Login</li>
+          <ul className='flex space-x-4 items-center'>
+            <li>
+              <button className='bg-dark-subtle p-1 rounded'>
+                <BsFillSunFill className='text-secondary' size={24} />
+              </button>
+            </li>
+            <li>
+              <input
+                type='text'
+                className='border-2 border-dark-subtle rounded p-1 bg-transparent text-xl outline-none focus:border-white transition text-white'
+                placeholder='Search...'
+              />
+            </li>
+            <li className='text-white font-semibold text-lg'>Login</li>
           </ul>
         </div>
       </div>
