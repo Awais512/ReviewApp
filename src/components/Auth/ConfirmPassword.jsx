19381502582ad5f411075c757f2a6e7cb04df42f
awais512 <raza.awais25@gmail.com>
2022-06-11 16:17:01 +0500

    Confirm Password Form

--- src/components/Auth/ConfirmPassword.jsx
+++ src/components/Auth/ConfirmPassword.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Title, Container, FormInput, Submit } from '../';
 
 const ConfirmPassword = () => {
-  return <div>ConfirmPassword</div>;
+  return (
+    <>
+      <div className='fixed inset-0 bg-primary -z-20 flex justify-center items-center'>
+        <Container>
+          <form className='bg-secondary rounded p-6 w-96 space-y-6'>
+            <Title>Please Enter new Password</Title>
+
+            <FormInput
+              label='New Password'
+              placeholder='Enter Your New Password'
+              name='password'
+              type='password'
+            />
+            <FormInput
+              label='Confirm Password'
+              placeholder='Confirm Password'
+              name='confirmPassword'
+              type='password'
+            />
+            <Submit value='Confirm Password' />
+          </form>
+        </Container>
+      </div>
+    </>
+  );
 };
 
 export default ConfirmPassword;
