14dd703bbdfa30682e3cf4745f8aa72b00266289
awais512 <raza.awais25@gmail.com>
2022-06-12 18:05:48 +0500

    Toggling Theme

--- src/context/ThemeProvider.js
+++ src/context/ThemeProvider.js
@@ -1,20 +1,36 @@
-import { useEffect } from 'react';
-import { createContext } from 'react';
+import React, { createContext, useEffect } from 'react';
+
 export const ThemeContext = createContext();
+
+const defaultTheme = 'light';
+const darkTheme = 'dark';
 
 export default function ThemeProvider({ children }) {
   const toggleTheme = () => {
-    const toggle = document.documentElement.classList.add('dark');
-    localStorage.setItem('theme', 'dark');
+    const oldTheme = getTheme();
+    const newTheme = oldTheme === defaultTheme ? darkTheme : defaultTheme;
+
+    updateTheme(newTheme, oldTheme);
   };
 
   useEffect(() => {
-    const theme = localStorage.getItem('theme');
-    document.documentElement.classList.add(theme);
+    const theme = getTheme();
+    if (!theme) updateTheme(defaultTheme);
+    else updateTheme(theme);
   }, []);
+
   return (
     <ThemeContext.Provider value={{ toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
 }
+
+const getTheme = () => localStorage.getItem('theme');
+
+const updateTheme = (theme, themeToRemove) => {
+  if (themeToRemove) document.documentElement.classList.remove(themeToRemove);
+
+  document.documentElement.classList.add(theme);
+  localStorage.setItem('theme', theme);
+};
