66a5c24aff433b8d9f6629606b3eb8e27036a96b
awais512 <raza.awais25@gmail.com>
2022-06-14 09:53:00 +0500

    Signup State

--- src/components/Auth/Signup.jsx
+++ src/components/Auth/Signup.jsx
@@ -1,28 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormInput, Submit, Title, Container, CustomLink } from '../';
 import { commonModalClasses } from '../../utils/theme';
 import FormContainer from '../Form/FormContainer';
 
 const Signup = () => {
+  const [userInfo, setUserInfo] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const { name, email, password } = userInfo;
+
+  const handleChange = ({ target }) => {
+    const { value, name } = target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(userInfo);
+  };
   return (
     <FormContainer>
       <Container>
-        <form className={`${commonModalClasses} w-96 space-y-6`}>
+        <form
+          onSubmit={handleSubmit}
+          className={`${commonModalClasses} w-96 space-y-6`}
+        >
           <Title>Sign Up</Title>
 
           <FormInput
+            onChange={handleChange}
+            value={name}
             label='Name'
             placeholder='Enter Your Name'
             name='name'
             type='text'
           />
           <FormInput
+            onChange={handleChange}
+            value={email}
             label='Email'
             placeholder='Enter Your Email'
             name='email'
             type='text'
           />
           <FormInput
+            onChange={handleChange}
+            value={password}
             label='Password'
             placeholder='Enter Your Password'
             name='password'
