a549790dfa0a9c5777fed0149e422cedb258c93a
awais512 <raza.awais25@gmail.com>
2022-06-24 10:37:26 -0700

    Login on email verification

--- src/components/Auth/EmailVerification.jsx
+++ src/components/Auth/EmailVerification.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState, useRef } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import { Title, Container, Submit } from '../';
-import { verifyUserEmail } from '../../api/auth';
-import { useNotification } from '../../hooks';
+import { useEffect, useState, useRef } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Title, Container, Submit } from "../";
+import { verifyUserEmail } from "../../api/auth";
+import { useNotification, useAuth } from "../../hooks";
 
-import { commonModalClasses } from '../../utils/theme';
-import FormContainer from '../Form/FormContainer';
+import { commonModalClasses } from "../../utils/theme";
+import FormContainer from "../Form/FormContainer";
 
 const OTP_LENGTH = 6;
 
@@ -22,12 +22,14 @@ const isValidOTP = (otp) => {
 
 const EmailVerification = () => {
   const { updateNotification } = useNotification();
+  const { isAuth, authInfo } = useAuth();
+  const { isLoggedIn } = authInfo;
   const { state } = useLocation();
   const user = state?.user;
   const navigate = useNavigate();
   console.log(user);
 
-  const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(''));
+  const [otp, setOtp] = useState(new Array(OTP_LENGTH).fill(""));
   const [activeOtpIndex, setActiveOtpIndex] = useState(0);
 
   const inputRef = useRef();
@@ -56,7 +58,7 @@ const EmailVerification = () => {
   };
 
   const handleKeyDown = ({ key }, index) => {
-    if (key === 'Backspace') {
+    if (key === "Backspace") {
       focusPrevInputField(index);
     }
   };
@@ -64,14 +66,20 @@ const EmailVerification = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!isValidOTP(otp)) {
-      return updateNotification('error','Invalid OTP');
+      return updateNotification("error", "Invalid OTP");
     }
-    const { error, message } = await verifyUserEmail({
-      OTP: otp.join(''),
+    const {
+      error,
+      message,
+      user: userResponse,
+    } = await verifyUserEmail({
+      OTP: otp.join(""),
       userId: user.id,
     });
-    if (error) return updateNotification('error', error);
-    updateNotification('success', message);
+    if (error) return updateNotification("error", error);
+    updateNotification("success", message);
+    localStorage.setItem("auth-token", userResponse.token);
+    isAuth();
   };
 
   useEffect(() => {
@@ -80,9 +88,10 @@ const EmailVerification = () => {
 
   useEffect(() => {
     if (!user) {
-      navigate('/not-found');
+      navigate("/not-found");
     }
-  }, [user, navigate]);
+    if (isLoggedIn) navigate("/");
+  }, [user, navigate, isLoggedIn]);
 
   return (
     <>
@@ -91,25 +100,25 @@ const EmailVerification = () => {
           <form onSubmit={handleSubmit} className={`${commonModalClasses}`}>
             <div>
               <Title>Please Enter the OTP to Verify Your Email</Title>
-              <p className='text-center dark:text-dark-subtle text-light-subtle'>
+              <p className="text-center dark:text-dark-subtle text-light-subtle">
                 OTP has been Sent to Your Email
               </p>
             </div>
-            <div className='flex justify-center items-center space-x-4'>
+            <div className="flex justify-center items-center space-x-4">
               {otp.map((_, index) => (
                 <input
                   ref={activeOtpIndex === index ? inputRef : null}
                   key={index}
-                  type='number'
-                  value={otp[index] || ''}
+                  type="number"
+                  value={otp[index] || ""}
                   onChange={(e) => handleOtpChange(e, index)}
                   onKeyDown={(e) => handleKeyDown(e, index)}
-                  className='w-12 h-12 border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary rounded bg-transparent outline-none dark:text-white text-primary text-center font-semibold text-xl spin-button-none firefox'
+                  className="w-12 h-12 border-2 dark:border-dark-subtle border-light-subtle dark:focus:border-white focus:border-primary rounded bg-transparent outline-none dark:text-white text-primary text-center font-semibold text-xl spin-button-none firefox"
                 />
               ))}
             </div>
 
-            <Submit value='Verify Account' />
+            <Submit value="Verify Account" />
           </form>
         </Container>
       </FormContainer>
